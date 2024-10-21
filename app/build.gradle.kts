9ba79316bdd1c2150575a2d5f442994f574f639c
Nguyen Van Tuong Nhat <tuongnhatnv@gmail.com>
2024-10-21 10:53:59 +0700

    create layout recycler-view-cardview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(libs.recyclerview.selection)
+    implementation(libs.material.v110)
 }
